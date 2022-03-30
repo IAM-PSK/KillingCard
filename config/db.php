45727555af91afb287a4b3e516ab5bfa99d39afa
I'mPSK <32847514+kongsaza1133@users.noreply.github.com>
2022-03-31 00:49:55 +0700

    Add files via upload
    อะไรวะ 2

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+    $servername="localhost";
+    $username = "root";
+    $password="";
+    $db="quilling_card";
+ 
+        $conn = new mysqli($servername,$username,$password,$db);
+      
+        echo ""; //Connected Successfully
+
+?>
